5e7b07d5df6314f5abef941e59391cc2d0d8119b
UmaruChawn <147772681+UmaruChawn@users.noreply.github.com>
2026-07-15 18:39:39 +0000

    Hopefully Final image test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ type AllPostsData = {
 }[]
 
 // Change this url to change the image!
-const imageUrl = '"C:\Users\Sheen\Downloads\Forest.jpg"';
+const imageUrl = '"/Images/Forest.jpg"';
 
 export default function Home() {
   const allPostsData: AllPostsData = getSortedPostsData()
